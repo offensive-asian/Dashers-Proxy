1d8c4dd7d1b867d78c082db3e4f41568e5880b6a
offensive-asian <jjjkim0183@gmail.com>
2026-02-26 09:01:49 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+//Credit Interstellar
+//EDIT THIS TO ADD PASSWORD PROTECTION
+const config = {
+    challenge: false, // Set to true if you want to enable password protection.
+    users: {
+      // You can add multiple users by doing username: 'password'.
+      ubx: "isdabest",
+    },
+  };
+  
+  export default config;
